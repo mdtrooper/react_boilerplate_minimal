e6dba6b3e1da7ac0dda3ab3b00f264381998ff29
Miguel de Dios <tres.14159@gmail.com>
2019-01-14 00:39:34 +0100

    Update the eslint and fixed the eslint warnings.

--- src/test.store.js
+++ src/test.store.js
@@ -1,19 +1,19 @@
 import { action, computed, observable } from 'mobx'
 
 class TestStore {
-    @observable things
+  @observable things
 
-    constructor() {
-        this.things = []
-    }
-    
-    @action add = (thing) => {
-        this.things.push(thing)
-    }
-    
-    @computed get Count() {
-        return this.things.length
-    } 
+  constructor () {
+    this.things = []
+  }
+
+  @action add = (thing) => {
+    this.things.push(thing)
+  }
+
+  @computed get Count () {
+    return this.things.length
+  }
 }
 
 const testStore = new TestStore()
