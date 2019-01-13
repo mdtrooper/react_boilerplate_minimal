733f8afc43a77bf1552d6b5583cc74df22c3e8e8
Miguel de Dios <tres.14159@gmail.com>
2019-01-14 00:31:20 +0100

    Starting with mobx.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Provider } from 'mobx-react'
 import { render } from 'react-dom'
+
+import * as store from './test.store'
 
 import App from './app'
 
 const root = document.getElementById('root')
 if (root) {
   render(<div>
-    <App text="Hello world" />
+    <Provider {...store}>
+      <App text="Hello world" />
+    </Provider>
     <h1>Goodbye world</h1>
   </div>, root)
 }
---
+++ src/test.store.js
@@ -0,0 +1,21 @@
+import { action, computed, observable } from 'mobx'
+
+class TestStore {
+    @observable things
+
+    constructor() {
+        this.things = []
+    }
+    
+    @action add = (thing) => {
+        this.things.push(thing)
+    }
+    
+    @computed get Count() {
+        return this.things.length
+    } 
+}
+
+const testStore = new TestStore()
+export default testStore
+export { TestStore }
