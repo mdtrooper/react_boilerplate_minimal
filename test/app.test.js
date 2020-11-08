63e3467b0bc63295bfd28bc3713e342444ac8f40
Miguel de Dios <tres.14159@gmail.com>
2020-11-08 13:40:08 +0100

    Fix old test.

--- test/app.test.js
+++ test/app.test.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import { Provider } from 'mobx-react'
+import store from '../src/test.store'
 import App from '../src/app'
 import renderer from 'react-test-renderer'
 
 test('App changes the content "<text> (mouse <0|1>)" when hovered', () => {
   const component = renderer.create(
-    <App text="Hello world" />
+    <Provider store={store} >
+      <App text="Hello world" />
+    </Provider>
   )
   let tree = component.toJSON()
+  let h1_title = tree['children'].filter(_ => _['type']  == 'h1').pop()
 
   // manually trigger the callback
-  tree.props.onMouseEnter()
+  h1_title.props.onMouseEnter()
   // re-rendering
   tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 
   // manually trigger the callback
-  tree.props.onMouseLeave()
+  h1_title = tree['children'].filter(_ => _['type']  == 'h1').pop()
+  h1_title.props.onMouseLeave()
   // re-rendering
   tree = component.toJSON()
   expect(tree).toMatchSnapshot()
